0dc690c83f06cc36bd44f640da8d48793729e820
okay <okay@chalk>
2018-06-07 11:05:52 -0700

    [loops] add drum and bass part from "play along" example

--- app/script.js
+++ app/script.js
@@ -328,7 +328,7 @@ document.addEventListener("DOMContentLoaded", function() {
       var normalizedPositions = normalizeFace(positions);
       var faceHeight = positions[7][1] - positions[33][1];
       var distanceFromScreen = faceHeight / vidHeight;
-      volume = distanceFromScreen.toFixed(2);
+      volume = distanceFromScreen.toFixed(2) * 4;
 
       POSITIONS = positions;
 
@@ -342,4 +342,111 @@ document.addEventListener("DOMContentLoaded", function() {
       POSITIONS = null;
     }
   }
+
+	function makeLoops() {
+			//DRUMS//
+			//and a compressor
+			var drumCompress = new Tone.Compressor({
+				"threshold" : -30,
+				"ratio" : 6,
+				"attack" : 0.3,
+				"release" : 0.1
+			}).toMaster();
+			var distortion = new Tone.Distortion({
+				"distortion" : 0.4,
+				"wet" : 0.4
+			});
+			//hats
+			var hats = new Tone.Player({
+				"url" : "./audio/505/hh.[mp3|ogg]",
+				"volume" : -10,
+				"retrigger" : true,
+				"fadeOut" : 0.05
+			}).chain(distortion, drumCompress);
+			var hatsLoop = new Tone.Loop({
+				"callback" : function(time){
+					hats.start(time).stop(time + 0.05);
+				},
+				"interval" : "16n",
+				"probability" : 0.8
+			}).start("1m");
+			//SNARE PART
+			var snare = new Tone.Player({
+				"url" : "./audio/505/snare.[mp3|ogg]",
+				"retrigger" : true,
+				"fadeOut" : 0.1
+			}).chain(distortion, drumCompress);
+			var snarePart = new Tone.Sequence(function(time, velocity){
+				snare.volume.value = Tone.gainToDb(velocity);
+				snare.start(time).stop(time + 0.1);
+			}, [null, 1, null, [1, 0.3]]).start(0);
+			var kick = new Tone.MembraneSynth({
+				"pitchDecay" : 0.01,
+				"octaves" : 6,
+				"oscillator" : {
+					"type" : "square4"
+				},
+				"envelope" : {
+					"attack" : 0.001,
+					"decay" : 0.2,
+					"sustain" : 0
+				}
+			}).connect(drumCompress);
+			var kickPart = new Tone.Sequence(function(time, probability){
+				if (Math.random() < probability){
+					kick.triggerAttack("C1", time);
+				}
+			}, [1, [1, [null, 0.3]], 1, [1, [null, 0.5]], 1, 1, 1, [1, [null, 0.8]]], "2n").start(0);
+
+			// BASS
+			var bass = new Tone.FMSynth({
+				"harmonicity" : 1,
+				"modulationIndex" : 3.5,
+				"carrier" : {
+					"oscillator" : {
+						"type" : "custom",
+						"partials" : [0, 1, 0, 2]
+					},
+					"envelope" : {
+						"attack" : 0.08,
+						"decay" : 0.3,
+						"sustain" : 0,
+					},
+				},
+				"modulator" : {
+					"oscillator" : {
+						"type" : "square"
+					},
+					"envelope" : {
+						"attack" : 0.1,
+						"decay" : 0.2,
+						"sustain" : 0.3,
+						"release" : 0.01
+					},
+				}
+			}).toMaster();
+			var bassPart = new Tone.Part(function(time, event){
+        console.log("TIME", time, event);
+				if (Math.random() < event.prob){
+					bass.triggerAttackRelease(event.note, event.dur, time);
+				}
+			}, [{time : "0:0", note : "C2", dur : "4n.", prob: 1}, {time : "0:2", note : "C2", dur : "8n", prob : 0.6},
+				{time : "0:2.6666", note : "C2", dur : "8n", prob : 0.4}, {time : "0:3.33333", note : "C2", dur : "8n", prob : 0.9},
+				{time : "1:0", note : "C2", dur : "4n.", prob : 1}, {time : "1:2", note : "C2", dur : "8n", prob : 0.6},
+				{time : "1:2.6666", note : "C2", dur : "8n", prob : 0.4}, {time : "1:3.33333", note : "E2", dur : "8n", prob : 0.9},
+				{time : "2:0", note : "F2", dur : "4n.", prob : 1}, {time : "2:2", note : "F2", dur : "8n", prob : 0.6},
+				{time : "2:2.6666", note : "F2", dur : "8n", prob : 0.4}, {time : "2:3.33333", note : "F2", dur : "8n", prob : 0.9},
+				{time : "3:0", note : "F2", dur : "4n.", prob : 1}, {time : "3:2", note : "F2", dur : "8n", prob : 0.6},
+				{time : "3:2.6666", note : "F2", dur : "8n", prob : 0.4}, {time : "3:3.33333", note : "B1", dur : "8n", prob : 0.9}]).start(0);
+			bassPart.loop = true;
+			bassPart.loopEnd = "4m";
+
+			bassPart.start(0);
+
+      Tone.Transport.start("+0.1");
+
+	}
+
+	makeLoops();
+
 });
